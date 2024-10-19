37f8b4ddbab2d4fbf8c0c7c65f75631c7fbbe770
Pavel Kadikov <143909949+showmeurback@users.noreply.github.com>
2024-10-19 11:43:10 +0300

    Add files via upload

---
+++ Lab2/Maintenance/Maintenance.cpp
@@ -0,0 +1,6 @@
+#include "Maintenance.h"
+#include <iostream>
+
+void Maintenance::performMaintenance(Aircraft aircraft) {
+    std::cout << "Performing maintenance on aircraft: " << aircraft.registrationNumber << std::endl;
+}
