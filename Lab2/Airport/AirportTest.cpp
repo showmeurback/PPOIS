9c2f9f117c5f9a7d22b8ef4b7bba8a12ef7c59bc
Pavel Kadikov <143909949+showmeurback@users.noreply.github.com>
2024-10-19 12:18:29 +0300

    Create AirportTest.cpp

---
+++ Lab2/Airport/AirportTest.cpp
@@ -0,0 +1,14 @@
+#include "D:\BSUIR\2 курс\ППОИС\ЛР 2\PPOISLab2\Airport.h"
+#include "D:\BSUIR\2 курс\ППОИС\ЛР 2\PPOISLab2\Airport.cpp" 
+
+TEST(AirportTest, Initialization) {
+    Airport airport("JFK International", "New York");
+    EXPECT_EQ(airport.name, "JFK International");
+    EXPECT_EQ(airport.location, "New York");
+    EXPECT_TRUE(airport.flights.empty());
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
